5cc2132ce9dbb44ed66610447a994b98d5a7e5e2
deepshah <deep.shah@vuclip.com>
2019-06-06 19:01:14 +0530

    change

--- src/components/HomeScreen/HomeScreen.js
+++ src/components/HomeScreen/HomeScreen.js
@@ -11,22 +11,21 @@ export default class HomeScreen extends Component {
       deferredPrompt = e;
       // Update UI to notify the user they can add to home screen
       document.querySelector(".add-button").style.display = "block";
-
-      document.querySelector(".add-button").addEventListener("click", e => {
-        // hide our user interface that shows our A2HS button
-        console.log("hellllloooo");
-        document.querySelector(".add-button").style.display = "none";
-        // Show the prompt
-        deferredPrompt.prompt();
-        // Wait for the user to respond to the prompt
-        deferredPrompt.userChoice.then(choiceResult => {
-          if (choiceResult.outcome === "accepted") {
-            console.log("User accepted the A2HS prompt");
-          } else {
-            console.log("User dismissed the A2HS prompt");
-          }
-          deferredPrompt = null;
-        });
+    });
+    document.querySelector(".add-button").addEventListener("click", e => {
+      // hide our user interface that shows our A2HS button
+      console.log("hellllloooo");
+      document.querySelector(".add-button").style.display = "none";
+      // Show the prompt
+      deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      deferredPrompt.userChoice.then(choiceResult => {
+        if (choiceResult.outcome === "accepted") {
+          console.log("User accepted the A2HS prompt");
+        } else {
+          console.log("User dismissed the A2HS prompt");
+        }
+        deferredPrompt = null;
       });
     });
   };
