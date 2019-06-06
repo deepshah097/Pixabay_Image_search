81b5b03f179ee9769265e3e3e89fced02a00bdc1
deepshah <deep.shah@vuclip.com>
2019-06-06 19:26:29 +0530

    changed in service worker

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 // import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
-import { withStyles } from '@material-ui/core/styles';
-import NavBar from './components/navbar/NavBar';
-import Search from './components/search/Search';
-
+import { withStyles } from "@material-ui/core/styles";
+import NavBar from "./components/navbar/NavBar";
+import Search from "./components/search/Search";
 
 export class App extends Component {
   render() {
-    return(
+    return (
       // <MuiThemeProvider>
-        <div>
-          <NavBar />
-          <Search />
-        </div>
-    //  </MuiThemeProvider>
+      <div>
+        {/* <NavBar /> */}
+        {/* <Search /> */}
+        <button id="btn-deep">MDP</button>
+      </div>
+      //  </MuiThemeProvider>
     );
   }
 }
--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -22,6 +22,38 @@ const isLocalhost = Boolean(
 
 export function register(config) {
   if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
+    let deferredPrompt;
+
+    window.addEventListener("beforeinstallprompt", e => {
+      console.log("inside the before install");
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
+      console.log("mayur is awesome", deferredPrompt);
+
+      // Update UI to notify the user they can add to home screen
+      // document.querySelector(".add-button").style.display = "block";
+    });
+
+    const btnAdd = document.querySelector("#btn-deep");
+    // Installation must be done by a user gesture! Here, the button click
+    btnAdd.addEventListener("click", e => {
+      // hide our user interface that shows our A2HS button
+      btnAdd.style.display = "none";
+      // Show the prompt
+      deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      deferredPrompt.userChoice.then(choiceResult => {
+        if (choiceResult.outcome === "accepted") {
+          console.log("User accepted the A2HS prompt");
+        } else {
+          console.log("User dismissed the A2HS prompt");
+        }
+        deferredPrompt = null;
+      });
+    });
+
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
