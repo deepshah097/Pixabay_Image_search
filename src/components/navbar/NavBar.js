2e9c5e2de4a412ac5a4fff12f36a6ddc7d886a9e
deepshah <deep.shah@vuclip.com>
2019-06-06 17:48:27 +0530

    add

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,27 +1,26 @@
-import React from 'react'
-import { AppBar, Toolbar, Typography } from '@material-ui/core'
-import { withStyles } from '@material-ui/core/styles';
-
+import React from "react";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
 
 const styles = {
   root: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   menuButton: {
     marginLeft: -18,
-    marginRight: 10,
-  },
+    marginRight: 10
+  }
 };
 function NavBar(props) {
   return (
     <AppBar position="static">
-        <Toolbar variant="dense">
-          <Typography variant="h6" color="inherit">
-            Pixabay Image Finder
-            <button className="add-button">Add to home screen</button>
-          </Typography>
-        </Toolbar>
-      </AppBar>
+      <Toolbar variant="dense">
+        <Typography variant="h6" color="inherit">
+          Pixabay Image Finder
+          <button className="add-button">Add to home screen</button>
+        </Typography>
+      </Toolbar>
+    </AppBar>
   );
 }
 export default withStyles(styles)(NavBar);
