ac3732f7fd1c73d6c81c08cc8baf12e391069f1b
deepshah <deep.shah@vuclip.com>
2019-06-07 10:17:45 +0530

    set interval time

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -24,14 +24,19 @@ export function register(config) {
   if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
     console.log("inside the service worker");
     let deferredPrompt;
-    window.addEventListener("appinstalled", () => {
-      console.log("app installed successfully");
-    });
-    if (window.matchMedia("(display-mode: standalone)").matches) {
-      console.log("display-mode is standalone");
-    } else {
-      console.log("not open in the standalone app");
-    }
+    setInterval(() => {
+      window.addEventListener("appinstalled", () => {
+        console.log("app installed successfully");
+      });
+    }, 5000);
+
+    setInterval(() => {
+      if (window.matchMedia("(display-mode: standalone)").matches) {
+        console.log("display-mode is standalone");
+      } else {
+        console.log("not open in the standalone app");
+      }
+    }, 5000);
 
     window.addEventListener("beforeinstallprompt", e => {
       console.log("inside the before install");
