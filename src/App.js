c366475ff7008669d4fbf45730a9a20d490a2424
deepshah <deep.shah@vuclip.com>
2019-06-07 11:38:33 +0530

    message change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ export class App extends Component {
         <button id="btn-deep">MDP</button>
         <div id="div-gotit">
           <label>
-            You have already installed the app go to setting and open the app
+            You have already installed the app go to crome setting and open the
+            app
           </label>
           <button id="btn-gotit">Got it</button>
         </div>
