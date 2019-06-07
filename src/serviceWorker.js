7114e98f06d40795bfcb0853033346b60e042906
deepshah <deep.shah@vuclip.com>
2019-06-07 10:07:56 +0530

    changes in the service worker

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -51,7 +51,12 @@ export function register(config) {
       // hide our user interface that shows our A2HS button
       btnAdd.style.display = "none";
       // Show the prompt
-      deferredPrompt.prompt();
+
+      if (deferredPrompt.prompt()) {
+        console.log("open deferredPrompt");
+      } else {
+        console.log("not open deferred");
+      }
       // Wait for the user to respond to the prompt
       deferredPrompt.userChoice.then(choiceResult => {
         if (choiceResult.outcome === "accepted") {
